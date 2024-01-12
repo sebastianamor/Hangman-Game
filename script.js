9a2692b6db6d75d87651ce654a4d14fd5a3992e6
¨Jane <smoreira11@hotmail.com>
2024-01-12 20:38:50 +0900

    meke it

---
+++ script.js
@@ -0,0 +1,3 @@
+const wordContainer = document.getElementById('wordContainer');
+const startButton = document.getElementById('startButton');
+const usedLettersElement = document.getElementById('usedLetters');
