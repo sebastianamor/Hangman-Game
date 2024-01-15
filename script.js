35026eaeb69f9ff4002fdd410b82cbd2ff98e46c
¨Jane <smoreira11@hotmail.com>
2024-01-15 22:49:32 +0900

    まだまだ

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const wordContainer = document.getElementById('wordContainer');
 const startButton = document.getElementById('startButton');
 const usedLettersElement = document.getElementById('usedLetters');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width  = 0;
+ctx.canvas.height = 0;
+
+const bodyParts = [
+    [4,2,1,1],
+    [4,3,1,2],
+    [3,5,1,1],
+    [5,5,1,1],
+    [5,3,1,1]
+];
+
+
+let selectdWord;
+let usedLetters;
+let mistakes;
+let hits;
+
+const startGame = () => {
+   usedLetters = [] ;
+   mistakes  = 0 ;
+   hits = 0 ;
+   wordContainer.innerHTML = '';
+   usedLettersElement.innerHTML = '';
+   startButton.style.display = 'none';
+};
+
+startButton.addEventListener('click', startGame);
